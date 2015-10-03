aa0fb9206c7f583b94ef40bb461ade819af7ff9a
Michael Wirth <mike.wirth@gmail.com>
2015-10-03 03:48:47 +0200

    Added more complex model with relationships

--- hotlikeme/hotlikeme.py
+++ hotlikeme/hotlikeme.py
@@ -1,14 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 from flask_potion import Api, ModelResource
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    __tablename__ = 'Users'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     age = db.Column(db.Integer)
+
+class Comparison(db.Model):
+    __tablename__ = 'Comparisons'
+
+    id = db.Column(db.Integer, primary_key=True)
+    evaluator_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+
+    male = db.relationship(User)
+    female = db.relationship(User)
+    outcome = db.Column(db.Enum("open", "equal", "male", "female"))
+
+    evaluator = db.relationship(User, backref=backref('comparisons', lazy='dynamic'))
 
 db.create_all()
 
